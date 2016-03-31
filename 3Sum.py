109070678e083b32f65a4d8568b1bb0e43145334
JohnTuUESTC <qiangtu_uestc@163.com>
2016-03-31 20:30:55 +0800

    优化 3 Sum

--- 3Sum.py
+++ 3Sum.py
@@ -13,46 +13,45 @@ class Solution(object):
         if len(nums) < 3:
             return []
 
-        target = 0 #表示目标值
+        target = 0 # 表示目标值
 
-        result = [] #存储返回的结果
+        result = [] # 存储返回的结果
 
         nums.sort()
 
         for i in range(0, len(nums) - 2):
-            if nums[i] > 0:
+            if nums[i] > 0: # 当一个数为正数时,如果有解的话,应该已经在负数中出现过了
                 break
 
-            if i > 0 and nums[i] == nums[i - 1]: # 说明这个解已经在之前出现过了
+            if i > 0 and nums[i] == nums[i - 1]: # 除去重复
                 continue
 
             target -= nums[i]
 
             first = i + 1
             last = len(nums) - 1
-            temp_result = []
 
             while first < last:
-                while i + 1 < first <= len(nums) - 1 and nums[first] == nums[first - 1]:
-                    first += 1
-
-                while 0 <= last < len(nums) - 1 and nums[last] == nums[last + 1]:
+                temp = nums[first] + nums[last]
+                if temp > target:
                     last -= 1
+                elif temp < target:
+                    first += 1
+                else:
+                    temp_list = []
+                    temp_list.append(nums[i])
+                    temp_list.append(nums[first])
+                    temp_list.append(nums[last])
+                    result.append(temp_list)
+                    # 去除重复值, 只有在得到一个解的时候才需要这个操作
+                    while first < last and nums[first] == nums[first + 1]:
+                        first += 1
 
-                if first < last:
-                    temp = nums[first] + nums[last]
-                    if temp > target:
+                    while first < last and nums[last] == nums[last - 1]:
                         last -= 1
-                    elif temp < target:
-                        first += 1
-                    else:
-                        temp_list = []
-                        temp_list.append(nums[i])
-                        temp_list.append(nums[first])
-                        temp_list.append(nums[last])
-                        result.append(temp_list)
-                        first += 1
-                        last -= 1
+
+                    first += 1
+                    last -= 1
 
             target += nums[i]
 
