6001321756a81be63f9c120767a5d3a593a2ad95
JohnTuUESTC <qiangtu_uestc@163.com>
2016-04-05 20:56:33 +0800

    优化 Divide Two Integers

--- DivideTwoIntegers.py
+++ DivideTwoIntegers.py
@@ -20,6 +20,12 @@ class Solution(object):
         if dividend == 0:
             return 0
 
+        if divisor == -1:
+            if dividend == -2147483648:
+                return 2147483647
+            else:
+                return -dividend
+
         negetive_flag = False
 
         if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
@@ -27,26 +33,25 @@ class Solution(object):
 
         result = 0  # 记录除法的结果
 
-        if dividend == -2147483648 and divisor == -1:
-            return 2147483647
-
         dividend = abs(dividend)
         divisor = abs(divisor)
         bit = 1
 
         # 不断将除数累加
-        while divisor < dividend:
+        while (divisor << 1) <= dividend:
             divisor <<= 1
             bit <<= 1
 
-        if divisor > dividend:
-            divisor >>= 1
-            bit >>= 1
-
         while bit >= 1:
-            if dividend >= divisor:
+            if dividend > divisor:
                 dividend -= divisor
                 result += bit
+            elif dividend == divisor:
+                result += bit
+                if negetive_flag:
+                    return -result
+                else:
+                    return result
 
             bit >>= 1
             divisor >>= 1
