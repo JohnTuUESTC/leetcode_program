545d9dcb105d1f16395d228a6aebcb956da409f9
JohnTuUESTC <qiangtu_uestc@163.com>
2016-04-08 11:38:15 +0800

    优化 Combination Sum

--- CombinationSum.py
+++ CombinationSum.py
@@ -13,36 +13,24 @@ class Solution(object):
         :rtype: List[List[int]]
         """
 
-        combination = [] # 存储所有可能的组合
-        solution = [] #存储当前的组合
+        combination = [] # 存储结果
+        solution = [] # 存储当前的组合
 
         candidates.sort()
 
-        if target < candidates[0] or target == 0 or len(candidates) == 0:
-            return []
-
-        self.find_combin(candidates, target, solution, combination, 0)
+        self.find_combin(candidates, target, solution, combination)
 
         return combination
 
-    #其中level表示当前从哪一个元素开始寻找
-    def find_combin(self, candidates, target, solution, combination, level):
-        #表示找到了一种组合
-        if target == 0 and solution not in combination:
-            combination.append(solution)
-            return
-        #表示不可能找到一种组合
-        if target < 0:
-            return
-
-        temp_sol = copy.deepcopy(solution)
-
-        for i in range(level, len(candidates)):
-            temp_sol.append(candidates[i])
-            target -= candidates[i]
-            self.find_combin(candidates, target, temp_sol, combination, i)
-            temp_sol = solution[0:len(temp_sol) - 1]
-            target += candidates[i]
+    def find_combin(self, candidates, target, solution, combination):
+        for i in range(len(candidates)):
+            if target < candidates[i]:
+                break
+            elif target == candidates[i]:
+                combination.append(solution + [candidates[i]])
+                break
+            else:
+                self.find_combin(candidates[i:], target - candidates[i], solution + [candidates[i]], combination)
 
 
 if __name__ == "__main__":
