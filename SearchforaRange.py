e6be8e76e9b4e9ce24e0b7b9baa4797021b30edc
JohnTuUESTC <qiangtu_uestc@163.com>
2016-04-08 10:00:50 +0800

    优化 Search for a Range

--- SearchforaRange.py
+++ SearchforaRange.py
@@ -16,16 +16,14 @@ class Solution(object):
         if len(nums) == 0:
             return [-1, -1]
 
-        if target not in nums:
-            return [-1, -1]
-
+        '''
         high = len(nums) - 1
         low = 0
-        target_index = 0
+        target_index = -1
 
         #利用二分法来寻找合适的下标
         while low <= high:
-            middle = int(math.ceil((high + low) // 2))
+            middle = (high + low) / 2
             if nums[middle] == target:
                 target_index = middle
                 break
@@ -34,31 +32,52 @@ class Solution(object):
             else:
                 low = middle + 1
 
+        if target_index == -1:
+            return [-1,-1]
+
         first_index = target_index #target下标区间的下界
         last_index = target_index #target下标区间的上界
 
         #向前和向后寻找target的范围
-        if nums[0] == target:
-            first_index = 0
-        else:
-            while first_index > 0:
-                if nums[first_index - 1] != target:
-                    break
-                first_index -= 1
+        while first_index > 0:
+            if nums[first_index] != nums[first_index - 1]:
+                break
+            first_index -= 1
 
-        if nums[len(nums) - 1] == target:
-            last_index = len(nums) - 1
-        else:
-            while last_index < len(nums) - 1:
-                if nums[last_index + 1] != target:
-                    break
-                last_index += 1
+        while last_index < len(nums) - 1:
+            if nums[last_index + 1] != nums[last_index]:
+                break
+            last_index += 1
 
         return [first_index, last_index]
+        '''
+        range_first = self.lower_bound(nums, target) # 求出target的第一个元素
+        range_last = self.lower_bound(nums, target + 1) # 求出target + 1的第一个元素
+
+        # 如果target不在nums中的话,直接返回[-1,-1]
+        if range_first < len(nums) and nums[range_first] == target:
+            return [range_first, range_last - 1]
+        else:
+            return [-1, -1]
+
+    # 计算出在list中一个元素第一次出现的位置
+    def lower_bound(self, nums, target):
+        first = 0
+        last = len(nums) - 1
+
+        while first <= last:
+            mid = (first + last) / 2
+
+            if nums[mid] >= target:
+                last = mid - 1
+            else:
+                first = mid + 1
+
+        return first
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.searchRange([1,2,3], 3))
+    print(sol.searchRange([2,2], 2))
 
 
 
