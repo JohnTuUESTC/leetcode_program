a7964596882ebeccf7b7e62885dcc8f0ed32b746
JohnTuUESTC <qiangtu_uestc@163.com>
2016-04-11 10:18:03 +0800

    完成 Jump Game II

--- JumpGameII.py
+++ JumpGameII.py
@@ -12,29 +12,6 @@ class Solution(object):
         :rtype: int
         """
         '''
-        超时
-        path = [0] #存储到达末尾的路径
-        min_step = [len(nums)] #存储最少使用的步数
-
-        self.find_jump_path(nums, path, min_step)
-
-        return min_step[0]
-
-    def find_jump_path(self, nums, path, min_step):
-        # 表示已经能够到达数组的末尾
-        if nums[path[len(path) - 1]] + path[len(path) - 1] >= len(nums) - 1 and len(path) < min_step[0]:
-            min_step[0] = len(path)
-            return
-
-        temp_path = copy.deepcopy(path)
-
-        for i in range(1, nums[temp_path[len(temp_path) - 1]] + 1):
-            if temp_path[len(temp_path) - 1] + i < len(nums):
-                temp_path.append(temp_path[len(temp_path) - 1] + i)
-                self.find_jump_path(nums, temp_path, min_step)
-                temp_path.pop()
-    '''
-
         if nums[0] == 0 or len(nums) == 0:
             return 0
 
@@ -50,8 +27,27 @@ class Solution(object):
                     break
 
         return min_step[len(nums) - 1]
+        '''
+
+        if len(nums) <= 1:
+            return 0
+
+        cur_longest = nums[0] # 表示当前步能够移动的最远距离
+        next_longest = nums[0] # 表示下一步能够移动的最远距离
+        step = 1
+
+        for i in range(1, len(nums)):
+            if i > cur_longest: # 当前位置已经超出了当前步能够到达的最远位置,需要增加一步
+                cur_longest = next_longest
+                step += 1
+
+            if i + nums[i] > next_longest:
+                next_longest = i + nums[i]
+
+            if cur_longest >= len(nums) - 1:
+                return step
 
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.jump([5,4,0,1,3,6,8,0,9,4,9,1,8,7,4,8]))
+    print(sol.jump([1,1,1,1]))
