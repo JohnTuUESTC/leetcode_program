decaeb5a3dedddd58f8f1fd3b19ba2d601ec84f7
JohnTuUESTC <qiangtu_uestc@163.com>
2016-04-27 10:20:32 +0800

    优化 Search a 2D Matrix

--- Searcha2DMatrix.py
+++ Searcha2DMatrix.py
@@ -11,33 +11,25 @@ class Solution(object):
         :rtype: bool
         """
 
-        if len(matrix) == 0:
-            return False
-
-        if target < matrix[0][0]:
-            return False
-
         row = len(matrix) - 1
         column = len(matrix[0]) - 1
 
-        if target > matrix[row][column]:
-            return False
-
-        target_row = 0 #表示target可能所在的行
+        target_row = -1 #表示target可能所在的行
 
         for i in range(len(matrix)):
             #target可能出现在某一行中
             if matrix[i][0] <= target <= matrix[i][column]:
                 target_row = i
                 break
-            #target不存在
-            if i > 0 and matrix[i - 1][column] < target < matrix[i][0]:
-                return False
 
-        for j in range(len(matrix[0])):
-            if matrix[target_row][j] == target:
-                return True
-        return False
+        if target_row == -1:
+            return False
+        else:
+            for j in range(column + 1):
+                if matrix[target_row][j] == target:
+                    return True
+                elif matrix[target_row][j] > target:
+                    return False
 
 if __name__ == "__main__":
     sol = Solution()
