7b657a7cfc0d714d279b98fe9ff2e8a935977a68
JohnTuUESTC <qiangtu_uestc@163.com>
2016-04-28 11:40:58 +0800

    完成 Word Search

--- WordSearch.py
+++ WordSearch.py
@@ -13,70 +13,44 @@ class Solution(object):
         :rtype: bool
         """
 
-        used_flag = [] #记录board中的字母是否已经被使用
         for i in range(len(board)):
-            temp = []
-            for j in range(len(board[i])):
-                temp.append(0)
-            used_flag.append(temp)
-
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                if board[i][j] == word[0]:
-                    temp_used_flag = copy.deepcopy(used_flag)
-                    temp_used_flag[i][j] = 1
-
-                    if self.FindWord(board, i - 1, j, word[1: len(word)], temp_used_flag):
-                        return True
-
-                    if self.FindWord(board, i, j - 1, word[1: len(word)], temp_used_flag):
-                        return True
-
-                    if self.FindWord(board, i + 1, j, word[1: len(word)], temp_used_flag):
-                        return True
-
-                    if self.FindWord(board, i, j + 1, word[1: len(word)], temp_used_flag):
-                        return True
-
-                    continue
-                else:
-                    continue
+            if word[0] in board[i]:
+                for j in range(len(board[0])):
+                    if board[i][j] == word[0]:  # 首先寻找到开始字符的位置
+                        if self.FindWord(board, i, j, word, 1, [[i, j]]):  # 再去寻找之后字符
+                            return True
 
         return False
 
-    #从board[m][n]位置开始,寻找单词word
-    def FindWord(self, board, m, n, word, used_flag):
-        #如果查找单词的长度为0, 表示已经查找完毕
-        if len(word) == 0:
+    # 从board[m][n]位置开始,寻找单词word的第index字符
+    # stack表示已经使用过的位置
+    def FindWord(self, board, m, n, word, index, stack):
+
+        # 如果查找单词的长度为0, 表示已经查找完毕
+        if len(word) == index:
             return True
 
-        #判断查找位置是否已经超出边界
-        if m < 0 or m >= len(board):
-            return False
-        if n < 0 or n >= len(board[m]):
-            return False
+        stack_1 = []  # 存储有可能出现字符的位置
 
-        if board[m][n] == word[0] and used_flag[m][n] == 0:
-            temp_used_flag = copy.deepcopy(used_flag)
-            temp_used_flag[m][n] = 1
+        # 向左搜寻
+        if n > 0 and board[m][n - 1] == word[index] and [m, n - 1] not in stack:
+            stack_1.append([m, n - 1])
 
-            #查找相邻区域
-            if self.FindWord(board, m - 1, n, word[1: len(word)], temp_used_flag):
-                return True
+        # 向右搜寻
+        if n < len(board[0]) - 1 and board[m][n + 1] == word[index] and [m, n + 1] not in stack:
+            stack_1.append([m, n + 1])
 
-            if self.FindWord(board, m, n - 1, word[1: len(word)], temp_used_flag):
-                return True
+        # 向下搜寻
+        if m < len(board) - 1 and board[m + 1][n] == word[index] and [m + 1, n] not in stack:
+            stack_1.append([m + 1, n])
 
-            if self.FindWord(board, m + 1, n, word[1: len(word)], temp_used_flag):
-                return True
+        # 向上搜寻
+        if m > 0 and board[m - 1][n] == word[index] and [m - 1, n] not in stack:
+            stack_1.append([m - 1, n])
 
-            if self.FindWord(board, m, n + 1, word[1: len(word)], temp_used_flag):
-                return True
-
-            return False
-        else:
+        if len(stack_1) == 0:  # 表示没有可能的搜索位置
             return False
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.exist(["CAA","AAA","BCD"], "AAB"))
+    print(sol.exist(["aa"], "aaa"))
