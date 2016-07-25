1e5774ff1372374161c58a2dc6fd26c43ba5c4b4
JohnTuUESTC <qiangtu_uestc@163.com>
2016-07-25 10:40:32 +0800

    优化 Majority Element

--- MajorityElement.py
+++ MajorityElement.py
@@ -9,7 +9,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
+        '''
         element_num = {} # 存储序列中每一个不同元素出现的次数
 
         for i in range(len(nums)):
@@ -21,3 +21,18 @@ class Solution(object):
         for i in element_num:
             if element_num[i] > len(nums) / 2:
                 return i
+        '''
+
+        number = nums[0] # 存储出现次数超过一半的元素
+        times = 1
+
+        for i in range(1, len(nums)):
+            if times == 0: # 出现次数超过一半的数字是使times变为1的最后一个数字
+                number = nums[i]
+                times = 1
+            elif nums[i] == number: # 如果当前的数字与存储的数字相同
+                times += 1
+            else:
+                times -= 1
+
+        return number
